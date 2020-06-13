d6d5153e3fb228c831aa889b4c5879ca5bba7d8e
Irfan Rafif <mirfanrafif17@gmail.com>
2020-06-13 15:03:44 +0700

    autoroles and say

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const discord = require('discord.js');
-const client = discord.Client();
+const discord = require("discord.js");
+const client = new discord.Client();
 
-client.on('ready', () => {
-  console.log('I am ready');
+const prefix = ".";
+
+client.on("ready", () => {
+  console.log("I am ready");
 });
 
-client.on('message', (message) => {
-  if (message.content === 'ping') {
-    message.channel.send('pong');
+//message
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content === `${prefix}ping`) {
+  } else {
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    if (command === "say") {
+      message.channel.send(args);
+    }
   }
 });
 
-client.login('NzIxMDIyNDU4NTA0Njc1NDE4.XuOeyg.sEeXOjKDGZi_hovH0VyXei9ubkM');
+//autoroles
+client.on("guildMemberAdd", async (member) => {
+  var role = member.guild.roles.cache.find((r) => r.name === "numpang lewat");
+  await member.roles.add(role.id);
+});
+
+client.login("NzIxMDIyNDU4NTA0Njc1NDE4.XuOeyg.sEeXOjKDGZi_hovH0VyXei9ubkM");
