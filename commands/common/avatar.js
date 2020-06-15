91bbab18dce57d2b8265febbd75136ebf037564b
Irfan Rafif <mirfanrafif17@gmail.com>
2020-06-15 14:56:38 +0700

    dah terakhir hari ini. jangan lupa ngegame

--- commands/common/avatar.js
+++ commands/common/avatar.js
@@ -13,12 +13,12 @@ module.exports = class extends Command {
 
   run(message) {
     var avatar;
-    if (message.mentions.users.first()) {
+    if (!message.mentions.users.first()) {
+      message.channel.send("Mention orangnya dong...");
+    } else {
       var user = message.mentions.users.first();
       avatar = new MessageAttachment(user.avatarURL());
       return message.channel.send(avatar);
-    } else {
-      message.channel.send("Mention orangnya dong...");
     }
   }
 };
