d04344f207fd346d2b9eca24edf74b20e94608cc
Irfan Rafif <mirfanrafif17@gmail.com>
2020-06-16 11:31:11 +0700

    anjing emang formatternya

--- commands/music/play.js
+++ commands/music/play.js
@@ -21,64 +21,64 @@ module.exports = class extends Command {
       var data = res.items[0];
 
       const { channel } = message.member.voice;
-    if (!channel)
-      return message.channel.send("Kamu belum gabung voice channel, nak...");
+      if (!channel)
+        return message.channel.send("Kamu belum gabung voice channel, nak...");
 
-    const serverQueue = message.client.queue.get(message.guild.id);
-    const songInfo = await ytdl.getInfo(
-      "https://www.youtube.com/watch?v=" + data.id.videoId
-    );
+      const serverQueue = message.client.queue.get(message.guild.id);
+      const songInfo = await ytdl.getInfo(
+        "https://www.youtube.com/watch?v=" + data.id.videoId
+      );
 
-    const song = {
-      id: songInfo.videoDetails.videoId,
-      title: Util.escapeMarkdown(songInfo.videoDetails.title),
-      url: songInfo.videoDetails.video_url,
-    };
+      const song = {
+        id: songInfo.videoDetails.videoId,
+        title: Util.escapeMarkdown(songInfo.videoDetails.title),
+        url: songInfo.videoDetails.video_url,
+      };
 
-    if (serverQueue) {
-      serverQueue.songs.push(song);
-      message.react(`✅`);
-      return;
-    }
-
-    const queueConstruct = {
-      textChannel: message.channel,
-      voiceChannel: channel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true,
-    };
-    message.client.queue.set(message.guild.id, queueConstruct);
-    queueConstruct.songs.push(song);
-
-    const play = async (song) => {
-      const queue = message.client.queue.get(message.guild.id);
-      if (!song) {
-        queue.voiceChannel.leave();
-        message.client.queue.delete(message.guild.id);
+      if (serverQueue) {
+        serverQueue.songs.push(song);
+        message.react(`✅`);
         return;
       }
 
-      const dispatcher = queue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-          queue.songs.shift();
-          play(queue.songs[0]);
-        })
-        .on("error", (error) => console.error(error));
-      dispatcher.setVolumeLogarithmic(queue.volume / 5);
-      message.react("🎶");
-    };
+      const queueConstruct = {
+        textChannel: message.channel,
+        voiceChannel: channel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+      };
+      message.client.queue.set(message.guild.id, queueConstruct);
+      queueConstruct.songs.push(song);
 
-    try {
-      const connection = await channel.join();
-      queueConstruct.connection = connection;
-      play(queueConstruct.songs[0]);
-    } catch (error) {
-      message.client.queue.delete(message.guild.id);
-      await channel.leave();
-    }
+      const play = async (song) => {
+        const queue = message.client.queue.get(message.guild.id);
+        if (!song) {
+          queue.voiceChannel.leave();
+          message.client.queue.delete(message.guild.id);
+          return;
+        }
+
+        const dispatcher = queue.connection
+          .play(ytdl(song.url))
+          .on("finish", () => {
+            queue.songs.shift();
+            play(queue.songs[0]);
+          })
+          .on("error", (error) => console.error(error));
+        dispatcher.setVolumeLogarithmic(queue.volume / 5);
+        message.react("🎶");
+      };
+
+      try {
+        const connection = await channel.join();
+        queueConstruct.connection = connection;
+        play(queueConstruct.songs[0]);
+      } catch (error) {
+        message.client.queue.delete(message.guild.id);
+        await channel.leave();
+      }
     });
   }
 };
