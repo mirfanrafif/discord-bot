fbdb42f5182690e914bfb5d26cd7ce7d878b1c25
Irfan Rafif <mirfanrafif17@gmail.com>
2020-08-25 11:58:44 +0700

    bedain seach sama play

--- commands/music/play.js
+++ commands/music/play.js
@@ -10,86 +10,65 @@ module.exports = class extends Command {
       name: "play",
       group: "music",
       memberName: "play",
-      description: "Ngeplay musik",
+      description: "Ngeplay musik pake link",
       guildOnly: true,
     });
   }
 
-  run(message) {
-    var youtube = new YouTube();
-    youtube.setKey(process.env.YOUTUBE_API_KEY);
-    youtube.search(message.argString, 2, async function (err, res) {
-      var data = res.items[0];
-      console.log(data);
+  async run(message) {
+    console.log(message.argString);
 
-      const { channel } = message.member.voice;
-      if (!channel)
-        return message.channel.send("Kamu belum gabung voice channel, nak...");
+    const song = {
+      id: data.id.videoId,
+      title: Util.escapeMarkdown(data.snippet.title),
+      url: "https://www.youtube.com/watch?v=" + data.id.videoId,
+    };
 
-      const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue) {
+      serverQueue.songs.push(song);
+      message.react(`✅`);
+      return;
+    }
 
-      var songInfo;
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: channel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true,
+    };
+    message.client.queue.set(message.guild.id, queueConstruct);
+    queueConstruct.songs.push(song);
 
-      if (message.argString.substring(0, 5) == "https") {
-        songInfo = message.argString;
-      } else {
-        songInfo = await ytdl.getInfo(
-          "https://www.youtube.com/watch?v=" + data.id.videoId
-        );
-      }
-
-      const song = {
-        id: data.id.videoId,
-        title: Util.escapeMarkdown(data.snippet.title),
-        url: songInfo.videoDetails.video_url,
-      };
-
-      if (serverQueue) {
-        serverQueue.songs.push(song);
-        message.react(`✅`);
+    const play = async (song) => {
+      const queue = message.client.queue.get(message.guild.id);
+      if (!song) {
+        queue.voiceChannel.leave();
+        message.client.queue.delete(message.guild.id);
         return;
       }
 
-      const queueConstruct = {
-        textChannel: message.channel,
-        voiceChannel: channel,
-        connection: null,
-        songs: [],
-        volume: 5,
-        playing: true,
-      };
-      message.client.queue.set(message.guild.id, queueConstruct);
-      queueConstruct.songs.push(song);
+      const dispatcher = queue.connection
+        .play(ytdl(song.url), {
+          quality: "highestaudio",
+        })
+        .on("finish", () => {
+          queue.songs.shift();
+          play(queue.songs[0]);
+        })
+        .on("error", (error) => console.error(error));
+      dispatcher.setVolumeLogarithmic(queue.volume / 5);
+      message.react("🎶");
+    };
 
-      const play = async (song) => {
-        const queue = message.client.queue.get(message.guild.id);
-        if (!song) {
-          queue.voiceChannel.leave();
-          message.client.queue.delete(message.guild.id);
-          return;
-        }
-
-        const dispatcher = queue.connection
-          .play(ytdl(song.url), {
-            quality: "highestaudio",
-          })
-          .on("finish", () => {
-            queue.songs.shift();
-            play(queue.songs[0]);
-          })
-          .on("error", (error) => console.error(error));
-        dispatcher.setVolumeLogarithmic(queue.volume / 5);
-        message.react("🎶");
-      };
-
-      try {
-        const connection = await channel.join();
-        queueConstruct.connection = connection;
-        play(queueConstruct.songs[0]);
-      } catch (error) {
-        message.client.queue.delete(message.guild.id);
-        await channel.leave();
-      }
-    });
+    try {
+      const connection = await channel.join();
+      queueConstruct.connection = connection;
+      play(queueConstruct.songs[0]);
+    } catch (error) {
+      message.client.queue.delete(message.guild.id);
+      await channel.leave();
+    }
   }
 };
